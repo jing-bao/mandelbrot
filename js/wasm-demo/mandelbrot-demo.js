0cd4ce53604542972405ce33e83282c77071dc83
jing.bao <jing.bao@intel.com>
2016-02-01 10:58:55 +0800

    Add normal/asm.js/wasm selection and separate nonSimdAsmjsModule

--- js/wasm-demo/mandelbrot-demo.js
+++ js/wasm-demo/mandelbrot-demo.js
@@ -6,15 +6,16 @@
 
 // global variables
 var animate        = false;
-var use_simd       = false;
+var choice         = "Normal JS";
 var max_iterations = 100;
+var HAS_WASM = typeof _WASMEXP_ !== 'undefined';
 
 // Polyfill and alerts
 if (typeof Math.fround == 'undefined') {
   Math.fround = function(x) { return x };
 }
-if (typeof SIMD == 'undefined') {
-  alert('SIMD not implemented in this browser. SIMD speedup button is disabled');
+if (!HAS_WASM) {
+  alert('WASM not implemented in this browser. WASM choice is disabled');
 }
 
 // Asm.js module buffer.
@@ -66,295 +67,16 @@ var canvas = function () {
 
 }();
 
-function asmjsModule (global, imp, buffer) {
-  "use asm"
-  var b8 = new global.Uint8Array(buffer);
-  var toF = global.Math.fround;
-  var i4 = global.SIMD.Int32x4;
-  var f4 = global.SIMD.Float32x4;
-  var i4add = i4.add;
-  var i4and = i4.and;
-  var i4ext = i4.extractLane;
-  var i4sel = i4.select;
-  var i4check = i4.check;
-  var f4add = f4.add;
-  var f4sub = f4.sub;
-  var f4mul = f4.mul;
-  var f4lessThanOrEqual = f4.lessThanOrEqual;
-  var f4splat = f4.splat;
-  var imul = global.Math.imul;
-  var b4 = global.SIMD.Bool32x4;
-  var b4any = b4.anyTrue;
-  const zero4 = i4(0,0,0,0), one4 = i4(1,1,1,1), two4 = f4(2,2,2,2), four4 = f4(4,4,4,4);
-
-  const mk0 = 0x007fffff;
-
-  function declareHeapLength() {
-    b8[0x00ffffff] = 0;
-  }
-
-  function mapColorAndSetPixel (x, y, width, value, max_iterations) {
-    x = x | 0;
-    y = y | 0;
-    width = width | 0;
-    value = value | 0;
-    max_iterations = max_iterations | 0;
-
-    var rgb = 0, r = 0, g = 0, b = 0, index = 0;
-
-    index = (((imul((width >>> 0), (y >>> 0)) + x) | 0) * 4) | 0;
-    if ((value | 0) == (max_iterations | 0)) {
-      r = 0;
-      g = 0;
-      b = 0;
-    } else {
-      rgb = ~~toF(toF(toF(toF(value >>> 0) * toF(0xffff)) / toF(max_iterations >>> 0)) * toF(0xff));
-      r = rgb & 0xff;
-      g = (rgb >>> 8) & 0xff;
-      b = (rgb >>> 16) & 0xff;
-    }
-    b8[(index & mk0) >> 0] = r;
-    b8[(index & mk0) + 1 >> 0] = g;
-    b8[(index & mk0) + 2 >> 0] = b;
-    b8[(index & mk0) + 3 >> 0] = 255;
-  }
-
-  function mandelPixelX4 (xf, yf, yd, max_iterations) {
-    xf = toF(xf);
-    yf = toF(yf);
-    yd = toF(yd);
-    max_iterations = max_iterations | 0;
-    var c_re4  = f4(0,0,0,0), c_im4  = f4(0,0,0,0);
-    var z_re4  = f4(0,0,0,0), z_im4  = f4(0,0,0,0);
-    var count4 = i4(0,0,0,0);
-    var z_re24 = f4(0,0,0,0), z_im24 = f4(0,0,0,0);
-    var new_re4 = f4(0,0,0,0), new_im4 = f4(0,0,0,0);
-    var i = 0;
-    var mb4 = b4(0,0,0,0);
-
-    c_re4 = f4splat(xf);
-    c_im4 = f4(yf, toF(yd + yf), toF(yd + toF(yd + yf)), toF(yd + toF(yd + toF(yd + yf))));
-
-    z_re4  = c_re4;
-    z_im4  = c_im4;
-
-    for (i = 0; (i | 0) < (max_iterations | 0); i = (i + 1) | 0) {
-      z_re24 = f4mul(z_re4, z_re4);
-      z_im24 = f4mul(z_im4, z_im4);
-
-      mb4 = f4lessThanOrEqual(f4add(z_re24, z_im24), four4);
-      // If all 4 values are greater than 4.0, there's no reason to continue.
-      if (!b4any(mb4))
-        break;
-
-      new_re4 = f4sub(z_re24, z_im24);
-      new_im4 = f4mul(f4mul(two4, z_re4), z_im4);
-      z_re4   = f4add(c_re4, new_re4);
-      z_im4   = f4add(c_im4, new_im4);
-      count4  = i4add(count4, i4sel(mb4, one4, zero4));
-    }
-    return i4check(count4);
-  }
-
-  function mandelColumnX4 (x, width, height, xf, yf, yd, max_iterations) {
-    x = x | 0;
-    width = width | 0;
-    height = height | 0;
-    xf = toF(xf);
-    yf = toF(yf);
-    yd = toF(yd);
-    max_iterations = max_iterations | 0;
-
-    var y = 0;
-    var ydx4 = toF(0);
-    var m4 = i4(0,0,0,0);
-
-    ydx4 = toF(yd * toF(4));
-    for (y = 0; (y | 0) < (height | 0); y = (y + 4) | 0) {
-      m4   = i4check(mandelPixelX4(toF(xf), toF(yf), toF(yd), max_iterations));
-      mapColorAndSetPixel(x | 0, y | 0,   width,     i4ext(m4, 0), max_iterations);
-      mapColorAndSetPixel(x | 0, (y + 1) | 0, width, i4ext(m4, 1), max_iterations);
-      mapColorAndSetPixel(x | 0, (y + 2) | 0, width, i4ext(m4, 2), max_iterations);
-      mapColorAndSetPixel(x | 0, (y + 3) | 0, width, i4ext(m4, 3), max_iterations);
-      yf = toF(yf + ydx4);
-    }
-  }
-
-  function mandel (width, height, xc, yc, scale, max_iterations) {
-    width = width | 0;
-    height = height | 0;
-    xc = toF(xc);
-    yc = toF(yc);
-    scale = toF(scale);
-    max_iterations = max_iterations | 0;
-
-    var x0 = toF(0), y0 = toF(0);
-    var xd = toF(0), yd = toF(0);
-    var xf = toF(0);
-    var x = 0;
-
-    x0 = toF(xc - toF(scale * toF(1.5)));
-    y0 = toF(yc - scale);
-    xd = toF(toF(scale * toF(3)) / toF(width >>> 0));
-    yd = toF(toF(scale * toF(2)) / toF(height >>> 0));
-    xf = x0;
-
-    for (x = 0; (x | 0) < (width | 0); x = (x + 1) | 0) {
-      mandelColumnX4(x, width, height, xf, y0, yd, max_iterations);
-      xf = toF(xf + xd);
-    }
-  }
-
-  return mandel;
-}
-
-function nonSimdAsmjsModule (global, imp, buffer) {
-  "use asm"
-  var b8 = new global.Uint8Array(buffer);
-  var toF = global.Math.fround;
-  var imul = global.Math.imul;
-
-  const mk0 = 0x007fffff;
-  function declareHeapLength() {
-    b8[0x00ffffff] = 0;
-  }
-
-  function mandelPixelX1 (xf, yf, yd, max_iterations) {
-    xf = toF(xf);
-    yf = toF(yf);
-    yd = toF(yd);
-    max_iterations = max_iterations | 0;
-
-    var z_re  = toF(0), z_im  = toF(0);
-    var z_re2 = toF(0), z_im2 = toF(0);
-    var new_re = toF(0), new_im = toF(0);
-    var count = 0, i = 0, mi = 0;
-
-    z_re  = xf;
-    z_im  = yf;
-
-    for (i = 0; (i | 0) < (max_iterations | 0); i = (i + 1) | 0) {
-      z_re2 = toF(z_re * z_re);
-      z_im2 = toF(z_im * z_im);
-
-      if (toF(z_re2 + z_im2) > toF(4))
-        break;
-
-      new_re = toF(z_re2 - z_im2);
-      new_im = toF(toF(z_re * toF(2)) * z_im);
-      z_re   = toF(xf + new_re);
-      z_im   = toF(yf + new_im);
-      count  = (count + 1) | 0;
-    }
-    return count | 0;
-  }
-
-  function mapColorAndSetPixel (x, y, width, value, max_iterations) {
-    x = x | 0;
-    y = y | 0;
-    width = width | 0;
-    value = value | 0;
-    max_iterations = max_iterations | 0;
-
-    var rgb = 0, r = 0, g = 0, b = 0, index = 0;
-
-    index = ((((imul((width >>> 0), (y >>> 0)) | 0) + x) | 0) * 4) | 0;
-    if ((value | 0) == (max_iterations | 0)) {
-      r = 0;
-      g = 0;
-      b = 0;
-    } else {
-      rgb = ~~toF(toF(toF(toF(value >>> 0) * toF(0xffff)) / toF(max_iterations >>> 0)) * toF(0xff));
-      r = rgb & 0xff;
-      g = (rgb >>> 8) & 0xff;
-      b = (rgb >>> 16) & 0xff;
-    }
-    b8[(index & mk0) >> 0] = r;
-    b8[(index & mk0) + 1 >> 0] = g;
-    b8[(index & mk0) + 2 >> 0] = b;
-    b8[(index & mk0) + 3 >> 0] = 255;
-  }
-
-  function mandelColumnX1 (x, width, height, xf, yf, yd, max_iterations) {
-    x = x | 0;
-    width = width | 0;
-    height = height | 0;
-    xf = toF(xf);
-    yf = toF(yf);
-    yd = toF(yd);
-    max_iterations = max_iterations | 0;
-
-    var y = 0, m = 0;
-
-    yd = toF(yd);
-    for (y = 0; (y | 0) < (height | 0); y = (y + 1) | 0) {
-      m = mandelPixelX1(toF(xf), toF(yf), toF(yd), max_iterations) | 0;
-      mapColorAndSetPixel(x | 0, y | 0, width, m, max_iterations);
-      yf = toF(yf + yd);
-    }
-  }
-
-  function mandelX1 (width, height, xc, yc, scale, max_iterations) {
-    width = width | 0;
-    height = height | 0;
-    xc = toF(xc);
-    yc = toF(yc);
-    scale = toF(scale);
-    max_iterations = max_iterations | 0;
-
-    var x0 = toF(0), y0 = toF(0), xd = toF(0), yd = toF(0), xf = toF(0);
-    var x = 0;
-
-    x0 = toF(xc - toF(scale * toF(1.5)));
-    y0 = toF(yc - scale);
-    xd = toF(toF(scale * toF(3)) / toF(width >>> 0));
-    yd = toF(toF(scale * toF(2)) / toF(height >>> 0));
-    xf = x0;
-
-    for (x = 0; (x | 0) < (width | 0); x = (x + 1) | 0) {
-      mandelColumnX1(x, width, height, xf, y0, yd, max_iterations);
-      xf = toF(xf + xd);
-    }
-  }
-
-  function mandel (width, height, xc, yc, scale, max_iterations) {
-    width = width | 0;
-    height = height | 0;
-    xc = toF(xc);
-    yc = toF(yc);
-    scale = toF(scale);
-    max_iterations = max_iterations | 0;
-
-    var x0 = toF(0), y0 = toF(0);
-    var xd = toF(0), yd = toF(0);
-    var xf = toF(0);
-    var x = 0;
-
-    x0 = toF(xc - toF(scale * toF(1.5)));
-    y0 = toF(yc - scale);
-    xd = toF(toF(scale * toF(3)) / toF(width >>> 0));
-    yd = toF(toF(scale * toF(2)) / toF(height >>> 0));
-    xf = x0;
-
-    for (x = 0; (x | 0) < (width | 0); x = (x + 1) | 0) {
-      mandelColumnX1(x, width, height, xf, y0, yd, max_iterations);
-      xf = toF(xf + xd);
-    }
-  }
-
-  return mandel;
-}
-
-var HAS_SIMD = typeof SIMD !== 'undefined';
-var mandelSimd = (HAS_SIMD && asmjsModule (this, {}, buffer)) || null;
 var mandelNonSimd = nonSimdAsmjsModule (this, {}, buffer);
 
-function drawMandelbrot (width, height, xc, yc, scale, use_simd) {
+function drawMandelbrot (width, height, xc, yc, scale) {
   logger.msg ("drawMandelbrot(xc:" + xc + ", yc:" + yc + ")");
-  if (use_simd && HAS_SIMD)
-    mandelSimd(width, height, xc, yc, scale, max_iterations);
-  else
+  if (choice == "WASM" && HAS_WASM)
+    console.log("WASM");
+  else if (choice == "ASM.JS")
     mandelNonSimd(width, height, xc, yc, scale, max_iterations);
+  else
+    console.log("Normal");
   canvas.update(buffer);
 }
 
@@ -379,7 +101,7 @@ function animateMandelbrot () {
     if (animate) {
       setTimeout(draw1, 1);
     }
-    drawMandelbrot (canvas.getWidth(), canvas.getHeight(), xc, yc, scale, use_simd);
+    drawMandelbrot (canvas.getWidth(), canvas.getHeight(), xc, yc, scale);
     if (scale < scale_end || scale > scale_start) {
       scale_step = -scale_step;
       xc_step = -xc_step;
@@ -415,20 +137,11 @@ function stop() {
   animate = false;
 }
 
-function simd() {
-  logger.msg("use SIMD clicked");
-  var $simd = $("#simd");
+function choose() {
+  choice = $('input[name=choose]:checked').val();
+  logger.msg(choice);
   var $info = $("#info");
-  if (!use_simd) {
-    use_simd = true;
-    $simd.text("Don't use SIMD");
-    $info.text("SIMD");
-  }
-  else {
-    use_simd = false;
-    $simd.text("Use SIMD");
-    $info.text("No SIMD");
-  }
+  $info.text(choice);
 }
 
 function main () {
@@ -436,12 +149,9 @@ function main () {
   canvas.init ("#mandel");
   $("#start").click (start);
   $("#stop").click (stop);
-  if (typeof SIMD === "undefined") {
-    $("#simd").addClass("btn-disable");
-  }
-  else {
-    $("#simd").click (simd);
-  }
+  $("input[name=choose]").click (choose);
+  if (!HAS_WASM)
+    $("#wasm").attr ('disabled', true);
   animateMandelbrot ();
 }
 
